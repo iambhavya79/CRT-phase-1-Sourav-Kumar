3aaa126253b648611bdec3d8ac15d4ee427b8d2c
iambhavya79 <iambhavya79@gmail.com>
2026-05-04 10:37:09 +0530

    add triangle3

--- Day 2/trianglestar.java
+++ Day 2/trianglestar.java
@@ -1,8 +1,8 @@
 public class trianglestar {
-  
+
     public static void main(String[] args) {
       for(int i=1;i<=5;i++){
-        for(int j=1;j<=5;j++){
+        for(int j=1;j<=i;j++){
             System.out.print("*");
         }
          System.out.println();
