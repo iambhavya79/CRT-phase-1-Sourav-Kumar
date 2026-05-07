ef44882df5290d6d92a63cac68aee4e62a6071f9
iambhavya79 <iambhavya79@gmail.com>
2026-05-07 17:10:49 +0530

    add books_class

---
+++ day6/books_class.java
@@ -0,0 +1,24 @@
+public class books_class {
+    public static void main(String[] args) {
+     Book b1 = new Book();
+     b1.getbook("david gogins life");
+     System.out.println(b1.name);
+     b1.getbook(599);
+     System.out.println(b1.cost);
+    }
+}
+class Book{
+    String name ; 
+    int cost ; 
+
+    void getbook(String newName){
+        name = newName;
+
+    }
+    void getbook(int newCost){
+        cost = newCost;
+
+    }
+}
+
+
