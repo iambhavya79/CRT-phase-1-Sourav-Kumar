aede79cb1c2555194de9e407d1436afeee42e2f2
iambhavya79 <iambhavya79@gmail.com>
2026-05-07 11:05:10 +0530

    add hierachial_inheritance.java

--- day 1/divisibleby2.java
+++ day 1/divisibleby2.java
@@ -1,7 +1,6 @@
 // wap java program that take an interger as 
 // a input from the user that take print 
-// all athe number to n that is divisble by 2 
-
+// all athe number to n that is divisble by 2 jdfhdj
 import java.util.Scanner;
 
 public class divisibleby2  
@@ -10,7 +9,7 @@ public class divisibleby2
     Scanner sc = new Scanner(System.in);
     System.out.println("enter a number");
     int n = sc.nextInt();
-      for(int i=1;i<=n;i++)
+      for(int i=1;i<=n;i++)  
 
       if(i%2==0)
       {
