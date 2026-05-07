308a5ce521153018bacecdedb11ea17e719a9a99
iambhavya79 <iambhavya79@gmail.com>
2026-05-07 17:14:34 +0530

    add cricket_info

---
+++ day6/cricket_info.java
@@ -0,0 +1,25 @@
+public class cricket_info {
+     public static void main(String[] args) {
+
+        
+        Cricketer c1 = new Cricketer("MS dhoni", 10800, 52.8);
+
+        System.out.println("Player Name : " + c1.name);
+        System.out.println("Runs : " + c1.runs);
+        System.out.println("Average : " + c1.average);
+    }
+}
+
+class Cricketer {
+
+    String name;
+    int runs;
+    double average;
+
+    
+    Cricketer(String name, int runs, double average) {
+        this.name = name;
+        this.runs = runs;
+        this.average = average;
+    }
+}
