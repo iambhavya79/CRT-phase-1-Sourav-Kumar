2597d115e3e6dc3f8b53d8011ea0233acf83d38b
iambhavya79 <iambhavya79@gmail.com>
2026-05-10 09:57:09 +0530

    commit

---
+++ day 10/string.java
@@ -0,0 +1,7 @@
+public class string {
+    public static void main(String[] args) {
+        String name = "Jecrc";
+        System.out.println(name);
+    }
+    
+}
---
+++ day 10/string2.java
@@ -0,0 +1,13 @@
+public class string2 {
+    public static void main(String[] args) {
+        String str1 = "hello ";
+        String str2 = "jecrc";
+        int n = str1.length();
+        System.out.println(n);
+        System.out.println(str1 + "student");
+        System.out.println(str1.isEmpty());
+        // java doesn't have pop_back
+        // java doesn't have direct swap function
+        // java doesn't have push_back function
+    }
+}
