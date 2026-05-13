5ec64115039365289b109f9e47aeaf923e3e6fa1
iambhavya79 <iambhavya79@gmail.com>
2026-05-13 08:47:38 +0530

    normal commit

---
+++ day 12/recusion/numfactorial.java
@@ -0,0 +1,22 @@
+package recusion;
+
+public class numfactorial {
+    public static int factorial(int n) {
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        int result = factorial(n);
+
+        System.out.println("Factorial of " + n + " = " + result);
+    }
+}
+
---
+++ day 12/recusion/sumofdigit.java
@@ -0,0 +1,14 @@
+package recusion;
+
+public class sumofdigit {
+    public static int sum(int n){
+        if(n == 0){
+            return 0;
+        }
+        return  (n % 10) + sum(n / 10);
+    }
+    
+public static void main(String[] args) {
+    int ans  = sum(6739429);
+    System.out.println(ans);
+}}
